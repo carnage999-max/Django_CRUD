5d961c8285eff55144e437e754884f99f173428c
akinolaakintunde <akinolaakintunde92@gmail.com>
2022-06-25 13:34:03 +0100

    Files add

---
+++ blog/form.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import GeeksModel
+
+
+# creating a form
+class GeeksForm(forms.ModelForm):
+
+	# create meta class
+	class Meta:
+		# specify model to be used
+		model = GeeksModel
+
+		# specify fields to be used
+		fields = "__all__"
